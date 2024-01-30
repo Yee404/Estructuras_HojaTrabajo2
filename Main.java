de6b8c043ccbd9572cd34c7cf047ad5fb5664777
Kapiven <pin231132@uvg.edu.gt>
2024-01-29 18:22:11 -0600

    Se agrega la opción para leer el archivo de texto en el main

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 /* se usa para implementar el menu aparte de mandar a llamar a la calculadora para que se puedan hacer los calculos  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     
     // Calculadora<Integer> stackInterno = new ICalculadora()<Integer>(0);// 
@@ -7,7 +11,23 @@ public class Main {
 
     public static void main(String[] args){
 
+        String archivo = "Estructuras_HojaTrabajo2/DATOS.txt";
+        String linea = null;
+
+        try {
+            FileReader fileReader = new FileReader(archivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((linea = bufferedReader.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo '" + archivo + "'");
+        }
     }
+       
 }
 
 
